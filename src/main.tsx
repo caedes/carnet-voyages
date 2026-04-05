1736dc288abb6ff4282889518092d979f9b60b78
caedes <laurentromain@gmail.com>
2026-04-06 00:04:40 +0200

    feat: configure Sentry error tracking with @sentry/react

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
+import * as Sentry from '@sentry/react'
 import { getRouter } from './router'
 import './styles.css'
+
+Sentry.init({
+  dsn: 'https://cc4032fc879a4628c450357484e984a1@o4511166335090688.ingest.de.sentry.io/4511166346297424',
+  sendDefaultPii: true,
+})
 
 const router = getRouter()
 
